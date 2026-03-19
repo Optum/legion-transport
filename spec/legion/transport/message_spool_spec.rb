4414e29bfb5518e627e4b8481c2ae605266fcf4d
Esity <matthewdiverson@gmail.com>
2026-03-19 17:55:58 -0500

    fix message spool spec mock for exchange_name_for_spool

--- spec/legion/transport/message_spool_spec.rb
+++ spec/legion/transport/message_spool_spec.rb
@@ -17,9 +17,10 @@ RSpec.describe Legion::Transport::Message, 'spool fallback' do
     msg = described_class.new(routing_key: 'test.run', function: 'test')
     exchange_mock = instance_double(Legion::Transport::Exchange)
     allow(msg).to receive(:exchange).and_return(exchange_mock)
-    allow(exchange_mock).to receive(:respond_to?).and_return(false)
-    allow(exchange_mock).to receive(:publish).and_raise(Bunny::ConnectionClosedError.new('closed'))
+    allow(exchange_mock).to receive(:respond_to?).with(:new).and_return(false)
+    allow(exchange_mock).to receive(:respond_to?).with(:name).and_return(true)
     allow(exchange_mock).to receive(:name).and_return('task')
+    allow(exchange_mock).to receive(:publish).and_raise(Bunny::ConnectionClosedError.new('closed'))
     allow(msg).to receive(:encode_message).and_return('{"test":true}')
 
     expect { msg.publish }.not_to raise_error
